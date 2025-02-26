0eed886318f1521964632826e773547065c5640c
rr76dd <fahadmohssin87@gmail.com>
2025-02-27 01:57:49 +0300

    Preparing the project env

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('🚀 Lammah Backend is Running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
